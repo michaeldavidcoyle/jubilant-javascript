90234486c2f7b8eadeac0b55ff5a8366cfaee966
Michael Coyle <michael.david.coyle4@gmail.com>
2021-12-15 21:15:39 -0600

    Styled the DOM to mimic the console.

--- js/console-css.js
+++ js/console-css.js
@@ -1,3 +1,17 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 
-colors.forEach(color => console.log(`%c${color}`, `color: ${color}`));
+const body = document.body;
+const boxes = [];
+
+colors.forEach(color => {
+    console.log(`%c${color}`, `color: ${color}`);
+
+    let box = document.createElement('div');
+    box.style.color = color;
+    box.style.borderBottom = '1px solid #555';
+    box.style.height = '1.5em';
+    box.style.padding = '0.25em';
+    box.style.paddingLeft = '2em';
+    box.innerText = color;
+    body.append(box);
+});
