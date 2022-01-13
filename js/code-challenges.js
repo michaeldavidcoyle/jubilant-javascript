a0d38749b3176ab95ce72ed8317ff74fca07771a
Michael Coyle <michael.david.coyle4@gmail.com>
2022-01-12 22:21:35 -0600

    Solved another coding challenge.

--- js/code-challenges.js
+++ js/code-challenges.js
@@ -105,3 +105,22 @@ function stringToCharCount(string, separator) {
 
 console.log(stringToCharCount('hello world', '-'));
 console.log(stringToCharCount('challenge', '/'));
+
+// Given a string of names like this: "Travis:Meyer;Gene:Carangal;Tom:Young;Jeff:Meyer", write
+// a function that makes the entire string uppercase and sorts it in alphabetical order by last
+// name. If the last names are the same sort them by the first name. Put the last name in front
+// of the first name, remove the colons and semicolons, put the names in parentheses and separate
+// the names with commas.
+//
+// Example:
+// The end string should look like this: "(CARANGAL, GENE)(MEYER, JEFF)(MEYER, TRAVIS)(YOUNG, TOM)"
+
+function formatNames(stringOfNames) {
+    return '(' + stringOfNames.toUpperCase()
+        .split(';')
+        .map(name => name.split(':').reverse().join(', '))
+        .sort()
+        .join(')(') + ')';
+}
+
+console.log(formatNames("Travis:Meyer;Gene:Carangal;Tom:Young;Jeff:Meyer"));
