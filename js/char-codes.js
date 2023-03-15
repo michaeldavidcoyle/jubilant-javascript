46130407deb7085cb052543b676cf8e05699fdd1
Michael Coyle <michael.david.coyle4@gmail.com>
2023-03-14 21:00:40 -0500

    add columns for various fonts

--- js/char-codes.js
+++ js/char-codes.js
@@ -4,11 +4,17 @@ const rowTemplate = document.querySelector('#row-template');
 for (let i = 32; i < 65536; i++) {
     let row = rowTemplate.content.cloneNode(true);
     let codeCol = row.querySelector('.char-code');
-    let charCol = row.querySelector('.char');
+    let charSans = row.querySelector('.char-sans');
+    let charSerif = row.querySelector('.char-serif');
+    let charMono = row.querySelector('.char-mono');
+    let charCursive = row.querySelector('.char-cursive');
     let char = String.fromCharCode(i);
 
     codeCol.innerText = i;
-    charCol.innerText = char;
+    charSans.innerText = char;
+    charSerif.innerText = char;
+    charMono.innerText = char;
+    charCursive.innerText = char;
 
     tbody.appendChild(row);
 }
